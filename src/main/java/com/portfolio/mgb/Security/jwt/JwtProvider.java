1038ef7c449af8ad857c06b350bb8b1f8222bb3f
TheBonii <lucasbonino5@hotmail.com>
2022-11-16 21:56:49 -0300

    make it better

--- src/main/java/com/portfolio/mgb/Security/jwt/JwtProvider.java
+++ src/main/java/com/portfolio/mgb/Security/jwt/JwtProvider.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-import static sun.jvm.hotspot.HelloWorld.e;
 
 @Component
 public class JwtProvider {
