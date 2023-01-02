de55e2eca70d65ccfcf274c3f5b618e6e1fbe42e
TheBonii <lucasbonino5@hotmail.com>
2023-01-02 18:32:37 -0300

    Actualizacion Koyeb

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas ")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendportfolio-1d835.web.app/")
 public class PersonaController {
 
     @Autowired
