8ae57a5c4710f6de5897f097e74362828d55ab0f
TheBonii <lucasbonino5@hotmail.com>
2023-01-04 11:03:52 -0300

    Actualizacion

--- src/main/java/com/portfolio/mgb/Security/Service/UsuarioService.java
+++ src/main/java/com/portfolio/mgb/Security/Service/UsuarioService.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.portfolio.mgb.Security.Service;
 
 import com.portfolio.mgb.Security.Entity.Usuario;
@@ -10,19 +15,18 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 public class UsuarioService {
-
     @Autowired
     iUsuarioRepository iusuarioRepository;
-
-    public Optional<Usuario> getByNombreUsuario(String nombreUsuario) {
+    
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
         return iusuarioRepository.findByNombreUsuario(nombreUsuario);
     }
-
-    public boolean existByNombreUsuario(String nombreUsuario) {
+    
+    public boolean existsByNombreUsuario(String nombreUsuario){
         return iusuarioRepository.existsByNombreUsuario(nombreUsuario);
     }
-
-    public boolean existByEmail(String email) {
+    
+    public boolean existsByEmail(String email){
         return iusuarioRepository.existsByEmail(email);
     }
     
